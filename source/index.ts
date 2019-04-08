77d193bc30618e10f0c086d9e401d9099cd7d533
Kai Kasugai <kk@formitas.de>
2019-04-08 17:09:01 +0200

    v1.2.0

---
+++ source/index.ts
@@ -0,0 +1,11 @@
+import removeFromIdBasedObject from "./removeFromIdBasedObject";
+import addToIdBasedObjectIfNotExists from "./addToIdBasedObjectIfNotExists";
+import addToIdBasedObjectOrMerge from "./addToIdBasedObjectOrMerge";
+import addToIdBasedObjectOrOverwrite from "./addToIdBasedObjectOrOverwrite";
+import toggleItemInArray from "./toggleItemInArray";
+
+module.exports.removeFromIdBasedObject = removeFromIdBasedObject;
+module.exports.addToIdBasedObjectIfNotExists = addToIdBasedObjectIfNotExists;
+module.exports.addToIdBasedObjectOrMerge = addToIdBasedObjectOrMerge;
+module.exports.addToIdBasedObjectOrOverwrite = addToIdBasedObjectOrOverwrite;
+module.exports.toggleItemInArray = toggleItemInArray;
